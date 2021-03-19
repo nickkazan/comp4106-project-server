6355047c62bcdc577dae1b1ed2d8e36cfdd3a6e3
Nick Kazan <nicholas.kazan1@gmail.com>
2021-03-19 10:49:21 -0400

    added list formatting and label scanning

--- ocr.py
+++ ocr.py
@@ -17,7 +17,7 @@ def convert_bytes_to_int(bytes):
 def scan_images(filename, max_images):
   images = []
   with open(filename, 'rb') as file:
-    magic_number = file.read(4) # file.read(n) will read the next n bytes
+    _ = file.read(4) # file.read(n) will read the next n bytes
     num_of_images = min(convert_bytes_to_int(file.read(4)), max_images)
     num_of_rows = convert_bytes_to_int(file.read(4))
     num_of_cols = convert_bytes_to_int(file.read(4))
@@ -32,13 +32,40 @@ def scan_images(filename, max_images):
           row.append(pixel)
         image.append(row)
       images.append(image)
-
     return images
 
+
+def scan_labels(filename, max_labels):
+  labels = []
+  with open(filename, 'rb') as file:
+    _ = file.read(4)
+    num_of_items = min(convert_bytes_to_int(file.read(4)), max_labels)
+    for _ in range(num_of_items):
+      label = convert_bytes_to_int(file.read(1))
+      labels.append(label)
+  return labels
+
+
+def format_list(data):
+  flat_list = []
+  for image in data:
+    flat_list.append([pixel for row in image for pixel in row])
+  return flat_list
+
+      
 
 def main():
   print('Chosen values: TRAIN_VALUES={}, TEST_VALUES={}, K={}'.format(TRAIN_VALUES, TEST_VALUES, K))
   training_dataset = scan_images(TRAIN_IMAGE_FILE, TRAIN_VALUES)
+  training_labels = scan_labels(TRAIN_LABEL_FILE, TRAIN_VALUES)
+  test_dataset = scan_images(TEST_IMAGE_FILE, TEST_VALUES)
+  test_labels = scan_labels(TEST_LABEL_FILE, TEST_VALUES)
+
+  flat_training_dataset = format_list(training_dataset)
+  flat_test_dataset = format_list(test_dataset)
+
+  # Next, we need to run our KNN or other algorithm on the dataset
+
 
 
 if __name__ == '__main__':
