c6a1b20af4773d0ff74ee82fabc81ebfd9d0ad71
Nick Kazan <nicholas.kazan1@gmail.com>
2021-03-25 11:33:48 -0400

    finished the KNN algorithm and added a drawn test

--- ocr.py
+++ ocr.py
@@ -1,17 +1,21 @@
+from PIL import Image, ImageFilter
 
 TEST_LABEL_FILE = './data/t10k-labels-idx1-ubyte'
 TEST_IMAGE_FILE = './data/t10k-images-idx3-ubyte'
 TRAIN_LABEL_FILE = './data/train-labels-idx1-ubyte'
 TRAIN_IMAGE_FILE = './data/train-images-idx3-ubyte'
 
+DRAWN_TEST_IMAGE = './data/test_image.png'
+
 # These values should be played around with
 K = 5
-TRAIN_VALUES = 1000
-TEST_VALUES = 100
+TRAIN_VALUES = 3000
+TEST_VALUES = 5
 
 
 def convert_bytes_to_int(bytes):
   return int.from_bytes(bytes, 'big')
+
 
 # Read image bytes and place them into a list to be returned
 def scan_images(filename, max_images):
@@ -34,7 +38,7 @@ def scan_images(filename, max_images):
       images.append(image)
     return images
 
-
+# Read label bytes and place them into a list to be returned
 def scan_labels(filename, max_labels):
   labels = []
   with open(filename, 'rb') as file:
@@ -46,26 +50,126 @@ def scan_labels(filename, max_labels):
   return labels
 
 
+# Extract features from our data, in our case flatten the 2D list
 def format_list(data):
   flat_list = []
   for image in data:
     flat_list.append([pixel for row in image for pixel in row])
   return flat_list
 
-      
+
+# Calculate the euclidean distance between the training data and the test data
+def euclidean_distance(a, b):
+  zipped_values = []
+  for cur_a, cur_b in zip(a, b):
+    if type(cur_b) == int:
+      zipped_values.append((convert_bytes_to_int(cur_a) - cur_b)**2)
+    else:
+      zipped_values.append((convert_bytes_to_int(cur_a) - convert_bytes_to_int(cur_b))**2)
+  return sum(zipped_values)**0.5
+
+
+# Determine which element appeared most frequently in the KNN
+def get_most_frequent_element(candidates):
+  return max(set(candidates), key=candidates.count)
+
+
+# Calculate the euclidean distance for the entire training dataset + test cases
+def calculate_distances_for_test(training_dataset, test):
+  distances = []
+  for data_point in training_dataset:
+    distances.append(euclidean_distance(data_point, test))
+  return distances
+
+
+# Our KNN algorithm, this can be subbed out for any other OCR algorithm
+def knn(training_dataset, training_labels, test_dataset, test_labels, k):
+  predictions = []
+  for index, test in enumerate(test_dataset):
+    training_distances = calculate_distances_for_test(training_dataset, test)
+    sorted_distances_tuples = sorted(enumerate(training_distances), key=lambda x: x[1])
+    
+    sorted_distances_indices = []
+    for distance in sorted_distances_tuples:
+      sorted_distances_indices.append(distance[0])
+
+    candidates = []
+    for index2 in sorted_distances_indices[:k]:
+      candidates.append(training_labels[index2])
+
+    print("Answer: {} ----- Index: {} ----- Candidates: {}".format(test_labels[index], index, candidates))
+    top_candidate = get_most_frequent_element(candidates)
+    print(" Guess: {}\n".format(top_candidate))
+    predictions.append(top_candidate)
+  return predictions
+
+
+# Determine how accurate our predictions were vs. the actual labels
+def check_accuracy(predictions, true_labels):
+  correct = 0
+  total = len(predictions)
+  for cur_prediction, cur_true_label in zip(predictions, true_labels):
+    if cur_prediction == cur_true_label:
+      correct += 1
+  return ((correct / total) * 100)
+
+
+# Takes a drawn image and will return the list of values for each pixel
+def prepare_drawn_image(path):
+  # Create image with black canvas (28 x 28)
+  image = Image.open(path).convert('L')
+  width = float(image.size[0])
+  height = float(image.size[1])
+  canvas = Image.new('L', (28, 28), (0))
+
+  # If image is not a sqaure, resize to fit width or height of 20 pixels
+  if width > height:
+    nheight = int(round((20.0 / width * height), 0))
+    if (nheight == 0):
+      nheight = 1
+
+    # Set width to 20 and height accordingly
+    resized_image = image.resize((20, nheight), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+    top_padding = int(round(((28 - nheight) / 2), 0))
+    # Pasting the image onto our canvas
+    canvas.paste(resized_image, (4, top_padding))
+  else:
+    nwidth = int(round((20.0 / height * width), 0))
+    if (nwidth == 0):
+      nwidth = 1
+    
+    # Set height to 20 and width accordingly
+    resized_image = image.resize((nwidth, 20), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+    left_padding = int(round(((28 - nwidth) / 2), 0))
+    # Pasting the image onto our canvas
+    canvas.paste(resized_image, (left_padding, 4))
+  
+  # Save the image for test viewing and return the list of values for calculation
+  canvas.save("./data/drawn_image.png")
+  return list(canvas.getdata())
+
 
 def main():
-  print('Chosen values: TRAIN_VALUES={}, TEST_VALUES={}, K={}'.format(TRAIN_VALUES, TEST_VALUES, K))
+  print('Chosen values: TRAIN_VALUES={}, TEST_VALUES={}, K={}\n'.format(TRAIN_VALUES, TEST_VALUES, K))
   training_dataset = scan_images(TRAIN_IMAGE_FILE, TRAIN_VALUES)
   training_labels = scan_labels(TRAIN_LABEL_FILE, TRAIN_VALUES)
   test_dataset = scan_images(TEST_IMAGE_FILE, TEST_VALUES)
   test_labels = scan_labels(TEST_LABEL_FILE, TEST_VALUES)
 
+
+  # MY TEST
+  # flat_test_dataset = [prepare_drawn_image(DRAWN_TEST_IMAGE)]
+  # test_labels = [1]
+
   flat_training_dataset = format_list(training_dataset)
   flat_test_dataset = format_list(test_dataset)
 
-  # Next, we need to run our KNN or other algorithm on the dataset
+  predictions = knn(flat_training_dataset, training_labels, flat_test_dataset, test_labels, K)
 
+  print("Guesses: {}".format(predictions))
+
+  accuracy = check_accuracy(predictions, test_labels)
+  print("Accuracy: {}%".format(accuracy))
 
 
 if __name__ == '__main__':
